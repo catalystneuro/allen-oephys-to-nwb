8af7749ee38281b805be20da242513dbfeb65ded
luiztauffer <luiz_tauffer@hotmail.com>
2020-06-23 20:03:23 +0700

    add trials, fix #2

--- allen_oephys_to_nwb/allen_oephys_to_nwb.py
+++ allen_oephys_to_nwb/allen_oephys_to_nwb.py
@@ -79,11 +79,14 @@ class AllenOephysNWBConverter(NWBConverter):
                 if tr == 'raw':
                     trace_data = np.squeeze(f['Vm'])
                     trace_name = 'raw_membrane_voltage'
+                    description = 'raw voltage trace'
                 if tr == 'filtered':
                     trace_data = np.squeeze(f['Vmfd'])
                     trace_name = 'filtered_membrane_voltage'
+                    description = 'voltage trace filtered between 250 Hz and 5 kHz'
                 electrical_series = ElectricalSeries(
                     name=trace_name,
+                    description=description,
                     data=trace_data,
                     electrodes=electrode_table_region,
                     starting_time=0.,
@@ -224,6 +227,36 @@ class AllenOephysNWBConverter(NWBConverter):
         with h5py.File(self.source_paths['path_processed'], 'r') as f:
             spike_times = np.where(f['spk'][0])[0] * f['dte'][0]
             self.nwbfile.add_unit(spike_times=spike_times)
+
+    def add_trials(self):
+        """Add trials data"""
+        with h5py.File(self.source_paths['path_processed'], 'r') as f:
+            if ('iStimOn' in f) and ('iStimOff' in f):
+                stim_on = f['iStimOn'][:][0]
+                stim_off = f['iStimOff'][:][0]
+                trial_times = np.hstack((stim_on.reshape(-1, 1), stim_off.reshape((-1, 1)))).reshape(-1) * f['dte'][0]
+                trial_times = np.append(trial_times, trial_times[-1] + 0.25)
+                sweep_order = np.squeeze(f['sweep_order'][:])
+                sweep_table = f['sweep_table'][:].T.reshape((-1, 4))
+                sweep_table = np.vstack((np.array([[np.nan, np.nan, np.nan, np.nan]]), sweep_table))
+
+                self.nwbfile.add_trial_column(name='orientation', description='description')
+                self.nwbfile.add_trial_column(name='phase', description='description')
+                self.nwbfile.add_trial_column(name='spatial_frequency', description='description')
+                self.nwbfile.add_trial_column(name='contrast', description='description')
+
+                for i, stim in enumerate(sweep_order):
+                    stim = int(stim + 1)
+                    self.nwbfile.add_trial(
+                        start_time=trial_times[i],
+                        stop_time=trial_times[i + 1],
+                        orientation=sweep_table[stim, 0],
+                        phase=sweep_table[stim, 1],
+                        spatial_frequency=sweep_table[stim, 2],
+                        contrast=sweep_table[stim, 3]
+                    )
+            else:
+                print('This file does not have stimulation data!')
 
 
 def convert2nwb(path_raw, path_tiff, path_processed, path_output):
