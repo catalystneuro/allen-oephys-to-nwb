54b41d3bff477ceaf88e618eaf8ecad6648012db
luiztauffer <luiz_tauffer@hotmail.com>
2020-07-22 12:47:13 +0700

    fix #3

--- allen_oephys_to_nwb/allen_oephys_to_nwb.py
+++ allen_oephys_to_nwb/allen_oephys_to_nwb.py
@@ -36,7 +36,7 @@ class AllenOephysNWBConverter(NWBConverter):
         meta_nwbfile = {
             'session_description': 'session description',
             'identifier': session_identifier,
-            'session_start_time': datetime.now(tzlocal()),
+            'session_start_time': datetime.strptime('1900-01-01 00:00:00', '%Y-%m-%d %H:%M:%S'),
             'pharmacology': subject_info['anesthesia'],
         }
         if metadata is None:
