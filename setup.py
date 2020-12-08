127485a622d6b342a670383b3183e54abb59aef8
luiz <luiz_tauffer@hotmail.com>
2020-12-08 21:28:14 +0100

    Update setup.py
    fix #8

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('requirements.txt') as f:
     install_requires = f.read().strip().split('\n')
 
 setup(
-    name='allen_to_nwb',
+    name='allen_oephys_to_nwb',
     version='0.1.0',
     description='NWB conversion scripts and tutorials.',
     long_description=long_description,
